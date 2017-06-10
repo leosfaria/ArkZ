60038d7ab0ae4e799c5cf125f4f686068e7a89c6
Leonardo <leofaria86@gmail.com>
2017-06-10 10:48:20 -0300

    primeiro commit do projeto + bloco

---
+++ Assets/BlockBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehaviour : MonoBehaviour {
+
+	public Material material;
+	public List<Material> randomMaterials;
+
+	// Use this for initialization
+	void Start () {
+		Transform blockVFX = this.transform.Find("VFX");
+
+		Renderer rend = blockVFX.GetComponent<Renderer>();
+
+		if (!material) {
+			int index = Random.Range(0, randomMaterials.Count);
+			material = randomMaterials[index];
+		}
+
+		rend.material = material;
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
